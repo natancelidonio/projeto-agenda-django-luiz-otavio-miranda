231c3b6a9b67234554e0e2159a33b554c87a6d9e
natancelidonio <natan.c.f.adm@gmail.com>
2025-07-26 10:24:54 -0300

    Customizando as opções de exibição na área administrativa do Django com admin.ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = 'first_name', 'id', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 20
+    list_editable = 'first_name',
