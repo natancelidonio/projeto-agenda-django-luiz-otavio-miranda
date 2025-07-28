4eb57a20ce321d84410ef3e71a5d17fa8a0867d3
natancelidonio <natan.c.f.adm@gmail.com>
2025-07-28 18:34:21 -0300

    ForeignKey - criando o Model Category e fazendo a ligação com  chave estrangeira

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -10,3 +10,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 20
     list_editable = 'phone',
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -7,6 +7,12 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f'{self.name} (ID: {self.id})'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -16,6 +22,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        blank=True,
+        null=True
+    )
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name} (ID: {self.id})'
