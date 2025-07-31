b97e793761c5c79b6f3fafe0117fb85997e695c3
natancelidonio <natan.c.f.adm@gmail.com>
2025-07-31 15:48:57 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
     
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos'
     }
     
     return render(
@@ -21,6 +22,7 @@ def contact(request, contact_id):
     
     context = {
         'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name}',
     }
     
     return render(
