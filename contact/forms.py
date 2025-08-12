0b817c1257de80ca1ee2e4ad630f7d731b4b060f
natancelidonio <natan.c.f.adm@gmail.com>
2025-08-12 19:07:39 -0300

    Configurando os campos e widgets do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -3,9 +3,29 @@ from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Digite seu nome aqui...'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Digite seu primeiro nome',
+    )
+    
+        
     class Meta:
         model = Contact
         fields= ('first_name', 'last_name', 'phone',)
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Digite seu nome'
+        #         }
+        #     ),
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
