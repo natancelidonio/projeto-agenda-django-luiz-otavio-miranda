9f740da0cf8d5ea438006bc760245a990633adbf
natancelidonio <natan.c.f.adm@gmail.com>
2025-08-13 14:55:59 -0300

    Validando campos do formulário com clean, clean_field e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -28,13 +28,31 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de Erro lero lero',
-                code='invalid'
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            self.add_error(
+                'last_name',
+                ValidationError(
+                    'O primeiro nome e o sobrenome não podem ser iguais.',
+                    code='invalid_name'
+                )
             )
-        )
+        
         return super().clean()
-    
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'O nome "ABC" não é permitido.',
+                    code='invalid_name'
+                )
+            )
+        
+        return first_name
