9e186af771711c80471617f4d27ba995895f5922
natancelidonio <natan.c.f.adm@gmail.com>
2025-08-27 15:13:38 -0300

    Validando campos de first_name, last_name e email do Usuário

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from contact.models import Contact
 
@@ -44,4 +45,31 @@ class ContactForm(forms.ModelForm):
         return first_name
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+    )
+    last_name = forms.CharField(
+        required=True,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+    
+    
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2',)
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError(
+                    'Este email já está em uso.',
+                    code='invalid_email'
+                )
+            )
+        return email
+        
